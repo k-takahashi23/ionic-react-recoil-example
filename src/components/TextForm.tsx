7a645edd7777a004aef5be7d89bb5b13e9a6be90
k-takahashi23 <efl.sad23@gmail.com>
2021-08-13 13:05:20 +0900

    MOD: inputForm

--- src/components/TextForm.tsx
+++ src/components/TextForm.tsx
@@ -1,25 +1,28 @@
+import { IonInput, IonItem } from "@ionic/react";
 import React, { useState } from "react";
 import { useSetRecoilState } from "recoil";
 import { tasksState } from '../recoil/atoms/tasksState';
 
 const TextForm: React.FC = () => {
-  const [title, setTitle] = useState<string>("");
+  const [inputTitle, setInputTitle] = useState<string>("");
   const [number, setNumber] = useState<number>(0);
   const setTasks = useSetRecoilState(tasksState);
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.target.value)
-  }
-
   const onAdd = () => {
     setNumber(number+1);
-    setTasks(t => [...t, { id: number, title, completed: false }])
-    setTitle("")
+    setTasks(t => [...t, { id: number, title: inputTitle, completed: false }]);
+    setInputTitle("");
   }
 
   return (
     <div>
-      <input type="text" value={title} onChange={onChange}></input>
+      <IonItem>
+        <IonInput
+          value={inputTitle}
+          placeholder="Enter Input"
+          onIonChange={e => setInputTitle(e.detail.value!)}
+        />
+      </IonItem>
       <button onClick={onAdd}>登録</button>
     </div>
   )
